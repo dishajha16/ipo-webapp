92a4087226671f2bc8547606e2a80bc28e4165ea
Vinay <151984389+vinayst04@users.noreply.github.com>
2025-01-31 00:41:49 +0530

    Delete js directory

--- js/community.js
+++
@@ -1,6 +0,0 @@
-
-    document.querySelector('.menu-btn').addEventListener('click', function () {
-        var navMenu = document.querySelector('nav ul');
-        navMenu.classList.toggle('nav-show'); // Toggle menu visibility
-    });
-
